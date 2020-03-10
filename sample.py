cbb8b3f7495e50f90c72676d057bc22480bae801
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-10 11:04:54 +0000

    changes to sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 def Fizzbuzz(value):
-    string = ''
-    if value%5 == 0:
-        string += 'Fizz'
-    if value%3 == 0:
-        string += 'Buzz'
-    return string
+    if value%5 == 0 and value%3 == 0:
+        return 'FizzBuzz'
 
-assert Fizzbuzz(15) == 'Fizz'
+assert Fizzbuzz(15) == 'FizzBuzz'
+print(Fizzbuzz(15))
+print(Fizzbuzz(5))
+print(Fizzbuzz(9))
+print(Fizzbuzz(2))
