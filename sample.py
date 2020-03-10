bb3944028387bc5054ec92ac3f6c2fc8bf61a0de
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-10 11:10:08 +0000

    Please enter the commit message for your changes. Lines starting

--- sample.py
+++ sample.py
@@ -3,6 +3,9 @@ def Fizzbuzz(value):
         return 'FizzBuzz'
     if value%5 == 0:
         return 'Buzz'
+    if value%3 == 0:
+        return 'Fizz'
 
 assert Fizzbuzz(15) == 'FizzBuzz'
 assert Fizzbuzz(5) == 'Buzz'
+assert Fizzbuzz(3) == 'Fizz'
