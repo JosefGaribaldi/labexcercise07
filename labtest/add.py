5630b4f19056b35c28acb25e47ddac71924181a3
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-26 21:02:01 +0000

    returns a additition of the string inputted in the method

--- labtest/add.py
+++ labtest/add.py
@@ -1,5 +1,16 @@
 def Add(numbers):
     if numbers == '':
         return 0
+    else:
+        total = 0
+        if ',' in numbers:
+            numbers = numbers.split(',')
+        for letter in numbers:
+            if letter.isdigit():
+                total += int(letter)
+            else:
+                pass
+        return total
 
 assert Add('') == 0
+assert Add('1,2') == 3
