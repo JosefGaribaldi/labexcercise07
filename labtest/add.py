04a1b039a353e2444f1dc55f82288fd23b7fd800
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-26 23:40:09 +0000

    I tried to make the assert for the raise but could not do so, I will fix it as a last resort

--- labtest/add.py
+++ labtest/add.py
@@ -1,8 +1,12 @@
+class Negative(Exception):
+    pass
+
 def Add(numbers):
     if numbers == '':
         return 0
     else:
         total = 0
+        string = 'Negatives not allowed:'
         if '\n' in numbers:
             numbers = numbers.replace('\n', ',')
         if ',' in numbers:
@@ -10,10 +14,18 @@ def Add(numbers):
         for letter in numbers:
             if letter.isdigit():
                 if int(letter) < 1000:
-                    total += int(letter)
+                    if int(letter) < 0:
+                        if string != 'Negatives not allowed:':
+                            string+= ','
+                        string += letter
+                    else:
+                        total += int(letter)
             else:
                 pass
-        return total
+        if string == 'Negatives not allowed:':
+            return total
+        else:
+            return Negative(string)
 
 assert Add('') == 0
 assert Add('1,2') == 3
@@ -22,3 +34,4 @@ assert Add('10,2,5,22,1,1') == 41
 assert Add('1\n2,3') == 6
 assert Add('1\n2\n3') == 6
 assert Add('1001,2') == 2
+assert Add('-1,2') == Negative('Negatives not allowed:-1')
