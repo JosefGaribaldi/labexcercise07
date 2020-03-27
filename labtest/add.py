d1e48d876999e44f165ed401e72b90ef305582af
Guðjón Karl Ólafsson <gudjono19@ru.is>
2020-03-27 01:41:48 +0000

    now we have added a better way of cleaning a string of all non digits and have a way of telling if the addition is legal or not

--- labtest/add.py
+++ labtest/add.py
@@ -7,25 +7,31 @@ def Add(numbers):
     else:
         total = 0
         string = 'Negatives not allowed:'
-        if '\n' in numbers:
-            numbers = numbers.replace('\n', ',')
-        if ',' in numbers:
-            numbers = numbers.split(',')
+        clean_string = ''
+        while numbers != '':
+            if numbers[0].isdigit() or numbers[0] == '-' and numbers[1].isdigit():
+                clean_string += numbers[0]
+            else:
+                if clean_string == '' or clean_string[-1] == ',' or len(numbers) == 1:
+                    pass
+                else:
+                    clean_string += ','
+            numbers = numbers[1:]
+        numbers = clean_string.split(',')
         for letter in numbers:
-            if letter.isdigit():
-                if int(letter) < 1000:
-                    if int(letter) < 0:
-                        if string != 'Negatives not allowed:':
-                            string+= ','
-                        string += letter
-                    else:
-                        total += int(letter)
+            if int(letter) < 1000:
+                if int(letter) < 0:
+                    if string != 'Negatives not allowed:':
+                        string+= ','
+                    string += letter
+                else:
+                    total += int(letter)
             else:
                 pass
         if string == 'Negatives not allowed:':
             return total
         else:
-            return Negative(string)
+            return string
 
 assert Add('') == 0
 assert Add('1,2') == 3
@@ -34,4 +40,7 @@ assert Add('10,2,5,22,1,1') == 41
 assert Add('1\n2,3') == 6
 assert Add('1\n2\n3') == 6
 assert Add('1001,2') == 2
-assert Add('-1,2') == Negative('Negatives not allowed:-1')
+assert Add("//X\n1X2") == 3
+assert Add("//%\n1%2%3") == 6 
+assert Add('-1,2') == 'Negatives not allowed:-1'
+assert Add("2,-4,3,-5") == 'Negatives not allowed:-4,-5'
